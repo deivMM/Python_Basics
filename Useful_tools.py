75e655262802e9bff9a3a9d7c1b04f10278aba55
David Marquez <Dmarquez@lortek.es>
2023-01-25 09:25:10 +0100

    usefull tools script update

--- Useful_tools.py
+++ Useful_tools.py
@@ -3,13 +3,17 @@
 # 
 # round_to
 # sort_list_of_fs_by_ascending_number
+# save_figs_as_pdf
+# save_imgs_as_pdf
 # 
 # =============================================================================
 
 import os
 import re
 import matplotlib.pyplot as plt
+from PIL import Image
 from math import log10, floor
+from matplotlib.backends.backend_pdf import PdfPages
 
 def round_to(num,n):
     '''
@@ -22,6 +26,51 @@ def sort_list_of_fs_by_ascending_number(list_of_fs):
     E.g.: ['Model_10.inp','Model_1.inp'] ---> ['Model_1.inp','Model_10.inp']
     '''
     list_of_fs.sort(key=lambda el:int(re.search('\d+',el).group()))
+
+def save_figs_as_pdf(path, f_list):
+    '''
+    This function generates a pdf with a given figure list.
+    ---
+    path = path where the pdf is to be saved 
+    f_list = figure list
+    '''
+    with PdfPages(path) as pdf:
+        for f in f_list:
+            pdf.savefig(f)
+
+def save_imgs_as_pdf(pdf_name = None, path = '.', r_pattern = 'png', remove_imgs = False):
+    '''
+    This function generates a pdf with the images contained in the folder.
+    ---
+    pdf_name = Name of the pdf to be saved with
+    path = path in which the images are contained 
+    r_pattern = regex pattern of the images 
+    remove_imgs = [T/F]
+    '''
+    if not pdf_name: pdf_name = path.split('/')[-1]
+    img_list = [f for f in os.listdir(path) if re.search(r_pattern, f)]
+    sort_list_of_fs_by_ascending_number(img_list)
+    if not img_list:
+        print('There is no image within the path with this pattern')
+        return
+    imgs = [Image.open(f'{path}/{f}').convert('RGB') for f in img_list]
+    imgs[0].save(f'{path}/{pdf_name}.pdf', 'PDF' ,resolution=100.0, save_all=True, append_images=imgs[1:])
+    if remove_imgs:[os.remove(f'{path}/{f}') for f in img_list]
+
+# =============================================================================
+
+f_list = []
+
+for i in range(10):
+    f, ax = plt.subplots(figsize=(10,10))
+    ax.plot([0,1],[0,i])
+    ax.set_ylim([-2,12])
+    plt.title(f'Grafico_{i+1}')
+    f_list.append(f)
+    plt.savefig(f'Imagen_{i+1}.png')
+
+save_figs_as_pdf('Graficas_desde_matplotlib.pdf', f_list)
+save_imgs_as_pdf('Graficas_desde_imagenes.pdf', remove_imgs = True)
 
 # =============================================================================
 
@@ -50,6 +99,7 @@ f, ax = plt.subplots()
 ax.plot([0,1,2,3,4],[0,10,12,15,16])
 ax.axvspan(0, 1, color='lightblue',alpha=.3)
 ax.axvspan(2,3, color='lightgrey',alpha=.3)
+ax.axhspan(2,4, color='lightgreen',alpha=.3)
 plt.show()
 
 # =============================================================================
